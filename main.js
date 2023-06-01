5a9927d9cdfc4c4b372b140a1d780d64e98952e8
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-06-01 22:40:53 +0300

    added the middleware that hande outOffOffice times

--- main.js
+++ main.js
@@ -2,8 +2,25 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-const Port = 3007;
+const Port = 3008;
 const app = express();
+
+const handleOfficeHrs = (req, res, next) => {
+  const date = new Date();
+  const currentHour = date.getHours();
+
+  if (currentHour >= 9 && currentHour < 17) {
+    next();
+    return;
+  }
+
+  const data = fs.readFileSync("views/out.html");
+  const html = data.toString();
+
+  res.status(301).set("Content-Type", "text/html").send(html);
+};
+
+app.use(handleOfficeHrs);
 
 app.get("/", (req, res) => {
   const htmlPath = path.join(__dirname, "views", "home.html");
